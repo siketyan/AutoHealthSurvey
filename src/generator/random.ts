f4766db9fc956933e17f8eda9755c195630c1414
Naoki Ikeguchi <root@siketyan.dev>
2021-01-05 22:25:05 +0900

    ✨ Support decimal parts for random numbers

--- src/generator/random.ts
+++ src/generator/random.ts
@@ -3,12 +3,13 @@ import { IGenerator } from '.';
 type Options = {
   min: number,
   max: number,
+  decimal?: number,
 };
 
-export class RandomGenerator implements IGenerator<Options, number> {
+export class RandomGenerator implements IGenerator<Options, string> {
   readonly type = 'random';
 
-  generate(options: Options): number {
-    return Math.round(Math.random() * (options.max - options.min)) + options.min;
+  generate(options: Options): string {
+    return (Math.random() * (options.max - options.min) + options.min).toFixed(options.decimal ?? 0);
   }
 }
